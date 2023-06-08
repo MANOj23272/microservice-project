6ed1c5dd42c013fc1fddcb1df6881427e5bdc4f5
udayk <panjinimanojkumar@gmail.com>
2023-06-08 18:36:25 +0530

    creating employee and department microservices

---
+++ department-service/src/main/java/net/javaguides/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,44 @@
+package net.javaguides.departmentservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.javaguides.departmentservice.dto.DepartmentDto;
+import net.javaguides.departmentservice.service.DepartmentService;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+	
+	private DepartmentService departmentService;
+	
+	@PostMapping
+	public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody  DepartmentDto departmentDto){
+		DepartmentDto saveDepartment = departmentService.saveDepartment(departmentDto);
+		return new ResponseEntity<>(saveDepartment,HttpStatus.CREATED);
+	}
+	
+//	@GetMapping("{id}")
+//	public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("id") Long departmentId){
+//		DepartmentDto department = departmentService.getDepartment(departmentId);
+//		return new ResponseEntity<>(department,HttpStatus.OK);
+//	}
+	
+	@GetMapping("{department-Code}")
+	public ResponseEntity<DepartmentDto> getDepartmentByCode(@PathVariable("department-Code") String departmentCode){
+		DepartmentDto department = departmentService.getDepartmentByCode(departmentCode);
+		return new ResponseEntity<>(department,HttpStatus.OK);
+	
+		}
+}
---
+++ employee-service/src/main/java/net/javaguides/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package net.javaguides.employeeservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.service.EmployeeService;
+
+@RestController
+@RequestMapping("api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+	
+	@PostMapping
+	public ResponseEntity<EmployeeDto> createEmployee(@RequestBody  EmployeeDto employeeDto){
+		EmployeeDto saveEmployee = employeeService.saveEmployee(employeeDto);
+		return new ResponseEntity<EmployeeDto>(saveEmployee,HttpStatus.CREATED);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<EmployeeDto> getEmployee(@PathVariable("id")  Long id){
+		EmployeeDto getEmployee = employeeService.getEmployeeById(id);
+		return new ResponseEntity<EmployeeDto>(getEmployee,HttpStatus.OK);
+	}
+}
